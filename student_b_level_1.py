3c31a82400ffd99a12c13b36fed53290ae57577a
Minh Thao Hoang <s4168765@student.rmit.edu.au>
2025-06-14 16:49:25 +1000

    level 1 task b

--- student_b_level_1.py
+++ student_b_level_1.py
@@ -6,8 +6,14 @@ def get_page_html(form_data):
         <title>Database Web-App Demo</title>
     </head>
     <body>
-        <h1>Page 1B - Hello, World!</h1>
-        <p>This is the first dynamically generated page!</p>
+        <h1>About us</h1>
+        <p>Our purpose:</p>
+        <p>Our website was created to inform knowledge about the global weather and climate. </p>
+        <p>As climate change poses a major threat to ecosystems, communities, and economies. This website provides users resources about climate, fulfill their needs whether for ordianry weather info or insights about the climate.</p>
+
+        <p>How the site can be use? </p>
+        <p>Our website gives users free access to a diversity range of data in the form of graph, statistics, etc.</p>
+        <p>User can use these data for basic weather check, eduction or even insightful researches. The website gives users wide possibilities to work with our resources. </p>
         <p><a href="/">Go to Page 1A</a></p>
         <p><a href="/page2a">Go to Page 2A</a></p>
         <p><a href="/page3a">Go to Page 3A</a></p>
