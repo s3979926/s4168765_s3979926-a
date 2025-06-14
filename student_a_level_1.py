bf9edc74335d87334facd5b1927492c996a5f1b2
s3979926 <s3979926@rmit.edu.vn>
2025-06-14 17:29:44 +1000

    a1 1

--- student_a_level_1.py
+++ student_a_level_1.py
@@ -1,5 +1,15 @@
+import pyhtml
 def get_page_html(form_data):
     print("About to return page home page...")
+
+    # sql for AET lowest temperature
+    sql_query = """
+    SELECT DMY, Location, MaxTemp, MinTemp
+    FROM AET
+    WHERE MinTemp = (SELECT MIN(MinTemp) FROM AET);
+    """
+    results = pyhtml.get_results_from_query("database/BOM2.db",sql_query)
+
     page_html="""<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -8,6 +18,14 @@ def get_page_html(form_data):
     <body>
         <h1 style="text-align: center;">Weather Website</h1>
         <p style= "text-align: center;">Our website aim to help user know more about their weather and to have better choice</p>
+    """
+    if results:
+        for row in results:
+            page_html += f"""
+        <h3 style="text-align: center;">Lowest Temperature Recorded In AET</h3>
+        <p style="text-align: center;">{row}</p>"""
+
+    page_html += """
         <p><a href="/">Go to Page 1A</a></p>
         <p><a href="/page2a">Go to Page 2A</a></p>
         <p><a href="/page3a">Go to Page 3A</a></p>
