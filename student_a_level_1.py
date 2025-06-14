ef7ee205a78740bc2721c0325bb52c80f40c7dcd
s3979926 <s3979926@rmit.edu.vn>
2025-06-14 16:56:19 +1000

    a

--- student_a_level_1.py
+++ student_a_level_1.py
@@ -6,15 +6,15 @@ def get_page_html(form_data):
         <title>Database Web-App Demo</title>
     </head>
     <body>
-        <h1>Page 1A - Hello, World!</h1>
-        <p>This is the first dynamically generated page!</p>
+        <h1 style="text-align: center;">Weather Website</h1>
+        <p style= "text-align: center;">Our website aim to help user know more about their weather and to have better choice</p>
         <p><a href="/">Go to Page 1A</a></p>
         <p><a href="/page2a">Go to Page 2A</a></p>
         <p><a href="/page3a">Go to Page 3A</a></p>
         <p><a href="/page1b">Go to Page 1B</a></p>
         <p><a href="/page2b">Go to Page 2B</a></p>
         <p><a href="/page3b">Go to Page 3B</a></p>
-        <img src="images/rmit.png" style="width: 30%; height: auto;">
+        <img src="images/data snapshot.png" style="width: 30%; height: auto; display: block; margin: 0 auto;">
     </body>
     </html>
     """
