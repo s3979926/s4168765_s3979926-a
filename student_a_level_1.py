afafdf238a5779b59dd517d7ef95f73a336dacf5
s3979926 <s3979926@rmit.edu.vn>
2025-06-14 17:35:37 +1000

    as

--- student_a_level_1.py
+++ student_a_level_1.py
@@ -6,7 +6,7 @@ def get_page_html(form_data):
     sql_query = """
     SELECT DMY, Location, MaxTemp, MinTemp
     FROM AET
-    WHERE MinTemp = (SELECT MIN(MinTemp) FROM AET);
+    WHERE MinTemp = MIN(MinTemp);
     """
     results = pyhtml.get_results_from_query("database/BOM2.db",sql_query)
 
