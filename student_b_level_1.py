4f280fc95bde3f6ba040f8cd1711385def2fb54a
Minh Thao Hoang <s4168765@student.rmit.edu.au>
2025-06-22 16:15:22 +1000

    sua lv1b

--- student_b_level_1.py
+++ student_b_level_1.py
@@ -1,6 +1,7 @@
 def get_page_html(form_data):
     print("About to return page home page...")
-    page_html="""<!DOCTYPE html>
+    
+    page_html = f"""<!DOCTYPE html>
     <html lang="en">
     <head>
         <title>Database Web-App Demo</title>
@@ -14,6 +15,26 @@ def get_page_html(form_data):
         <h3>How the site can be use? </h3>
         <p>Our website gives users free access to a diversity range of data in the form of graph, statistics, etc.</p>
         <p>User can use these data for basic weather check, eduction or even insightful researches. The website gives users wide possibilities to work with our resources. </p>
+
+    <head>
+        <script>
+            function toggleImage(id) {{
+                var img = document.getElementById(id);
+                if (img.style.display === "none") {{img.style.display = "block";}} 
+
+                else {{img.style.display = "none";}} }}
+
+        </script>
+    </head>
+    <body>
+
+        <h3>Our personas</h3>
+        <button onclick="toggleImage('personaA')">Show/Hide Persona A</button><br>
+        <img id="personaA" src="images/Persona_A" alt="Persona A" style="width:40%; height:auto; display:none;"><br><br>
+
+        <button onclick="toggleImage('personaB')">Show/Hide Persona B</button><br>
+        <img id="personaB" src="images/Persona_B" alt="Persona B" style="width:40%; height:auto; display:none;"><br>
+
         <p><a href="/">Go to Page 1A</a></p>
         <p><a href="/page2a">Go to Page 2A</a></p>
         <p><a href="/page3a">Go to Page 3A</a></p>
